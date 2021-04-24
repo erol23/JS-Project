2fa7d54381c2233209736422deb51944b61547c9
erol <hurkanrol15@gmail.com>
2021-04-24 17:49:04 +0300

    add html and css part

---
+++ randomQuotes/app.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('btn');
+const sents = document.getElementById('sentence');
+const name = document.getElementById('name');
+
+
+button.addEventListener('click', () => {
+    console.log('click me');
+})
