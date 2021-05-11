d908816b9855f80945f28d0be5a63b57ead9d85b
erol <hurkanrol15@gmail.com>
2021-05-11 22:23:53 +0300

    modified message page

--- theMessageProject/app.js
+++ theMessageProject/app.js
@@ -2,23 +2,22 @@ const msg = document.querySelector('#message');
 const lastMessage = document.querySelector('#lastmessage');
 const button = document.querySelector('button')
 
-
 function messageStyle(){
     lastMessage.style.display = "block";
 }   
-function display(){
+function display(){ 
     lastMessage.style.display = 'none'
 }
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    lastMessage.style = messageStyle();
+    lastMessage.style = messageStyle;
     console.log(msg.value);
     if(msg.value.trim() == ''){
         lastMessage.style.color = 'red'
         lastMessage.style.backgroundColor = 'rgb(173, 102, 102)'
         lastMessage.textContent = 'please enter a message!!'.toUpperCase();
-        setTimeout(display, 2000);
+        setTimeout(display,2000);
     }else{
         lastMessage.textContent = msg.value.toUpperCase();
     }
