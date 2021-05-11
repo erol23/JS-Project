e7bd7e934a1909fcd06bdf6fcad9e7a7ef9234ff
erol <hurkanrol15@gmail.com>
2021-05-11 16:28:13 +0300

    add click event

--- randomQuotes/app.js
+++ randomQuotes/app.js
@@ -1,8 +1,41 @@
 const button = document.getElementById('btn');
 const sents = document.getElementById('sentence');
-const name = document.getElementById('name');
+const author = document.getElementById('name');
+const quotes = [
+    {
+      quote:
+        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
+      author: " Life"
+    },
+    {
+      quote:
+        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+      author: "John Kenneth Galbraith"
+    },
+    {
+      quote:
+        "God save me from my friends. I can protect myself from my enemies.",
+      author: "Claude Louis Hector de Villars "
+    },
+    {
+      quote: "The price of anything is the amount of life you exchange for it.",
+      author: "David Thoreau"
+    },
+    {
+      quote:
+        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+      author: "Charles Lindbergh"
+    },
+    {
+      quote:
+        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+      author: " Tyne Daly"
+    }
+  ];
 
 
 button.addEventListener('click', () => {
-    console.log('click me');
+    let change = Math.floor(Math.random()*quotes.length)
+    sents.innerHTML=`<i class="fas fa-quote-left"></i>` + quotes[change].quote;
+    author.textContent=quotes[change].author;
 })
